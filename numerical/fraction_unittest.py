cd2c442880e1e650f5b881aa668033bdb6bba24f
ArtemJustArtem <ashkyl97@gmail.com>
2023-07-27 16:05:44 +0300

    placeholders and tests added

--- numerical/fraction_unittest.py
+++ numerical/fraction_unittest.py
@@ -1,8 +1,104 @@
 import unittest
+from .fractions import Fraction
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_zero_in_denominator(self):
+        with self.assertRaises(ZeroDivisionError):
+            _ = Fraction(5, 0)
+
+    def test_invalid_type_in_numerator(self):
+        with self.assertRaises(TypeError):
+            _ = Fraction('1', 1)
+
+    def test_invalid_type_in_denominator(self):
+        with self.assertRaises(TypeError):
+            _ = Fraction(1, '1')
+
+    def test_str_both_positive(self):
+        test = Fraction(2, 7)
+        result = str(test)
+        expected = '2/7'
+        self.assertEqual(result, expected)
+
+    def test_str_one_negative(self):
+        test = Fraction(2, -7)
+        result = str(test)
+        expected = '-2/7'
+        self.assertEqual(expected, result)
+
+    def test_str_both_negative(self):
+        test = Fraction(-2, -7)
+        result = str(test)
+        expected = '2/7'
+        self.assertEqual(expected, result)
+
+    def test_float(self):
+        test = Fraction(2, 7)
+        result = float(test)
+        expected = 2 / 7
+        self.assertEqual(expected, result)
+
+    def test_int(self):
+        test = Fraction(2, 7)
+        result = int(test)
+        expected = int(2 / 7)
+        self.assertEqual(expected, result)
+
+    def test_simplify_floats(self):
+        test = Fraction(1, 0.5)
+        result = test.simplify()
+        expected = Fraction(2, 1)
+        self.assertEqual(expected, result)
+
+    def test_simplify_ints(self):
+        test = Fraction(17, 34)
+        result = test.simplify()
+        expected = Fraction(1, 2)
+        self.assertEqual(expected, result)
+
+    def test_add(self):
+        a = Fraction(1, 2)
+        b = Fraction(3, 4)
+        result = a + b
+        expected = Fraction(5, 4)
+        self.assertEqual(expected, result)
+
+    def test_sub(self):
+        a = Fraction(1, 2)
+        b = Fraction(3, 4)
+        result = a - b
+        expected = Fraction(-1, 4)
+        self.assertEqual(expected, result)
+
+    def test_mul(self):
+        a = Fraction(4, 7)
+        b = Fraction(1, 2)
+        result = a * b
+        expected = Fraction(2, 7)
+        self.assertEqual(expected, result)
+
+    def test_truediv(self):
+        a = Fraction(4, 7)
+        b = Fraction(4, 2)
+        result = a / b
+        expected = Fraction(2, 7)
+        self.assertEqual(expected, result)
+
+    def test_eq(self):
+        a = Fraction(1, 2)
+        b = Fraction(2, 4)
+        result = (a == b)
+        expected = True
+        self.assertEqual(expected, result)
+
+    def test_ne(self):
+        a = Fraction(1, 2)
+        b = Fraction(2, 4)
+        result = (a != b)
+        expected = False
+        self.assertEqual(expected, result)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ numerical/fractions.py
@@ -0,0 +1,33 @@
+class Fraction:
+    def __init__(self, numerator: float | int = 0, denominator: float | int = 1):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __float__(self):
+        pass
+
+    def __int__(self):
+        pass
+
+    def simplify(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def __truediv__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __ne__(self, other):
+        pass
